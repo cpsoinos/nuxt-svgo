e6aa9ef8a6f03e49cec4853a9375dd268cf4279d
Corey Psoinos <coreypsoinos@gmail.com>
2022-05-01 20:12:39 -0400

    add rollup.emitCJS: true to build config

--- build.config.ts
+++ build.config.ts
@@ -3,5 +3,8 @@ import { defineBuildConfig } from "unbuild";
 export default defineBuildConfig({
   declaration: true,
   entries: ["src/module"],
+  rollup: {
+    emitCJS: true,
+  },
   externals: ["svgo", "@nuxt/kit", "@nuxt/types"],
 });
