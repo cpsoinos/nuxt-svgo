586ff2ccd4d444372333bede1fade35d700bc630
Corey Psoinos <coreypsoinos@gmail.com>
2022-05-21 17:00:39 -0400

    extend webpack config as well as vite

--- src/module.ts
+++ src/module.ts
@@ -1,26 +1,43 @@
-import { defineNuxtModule, addVitePlugin } from "@nuxt/kit";
-import { OptimizeOptions } from "svgo";
-import svgLoader from "vite-svg-loader";
+import { defineNuxtModule, addVitePlugin, extendWebpackConfig } from '@nuxt/kit'
+import { OptimizeOptions } from 'svgo'
+import svgLoader from 'vite-svg-loader'
 
 export interface ModuleOptions {
-  svgoConfig?: OptimizeOptions;
-  svgo?: boolean;
-  defaultImport?: "url" | "raw" | "component";
+  svgoConfig?: OptimizeOptions
+  svgo?: boolean
+  defaultImport?: 'url' | 'raw' | 'component'
 }
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: "nuxt-svgo",
-    configKey: "svgoOptions",
+    name: 'nuxt-svgo',
+    configKey: 'svgoOptions',
     compatibility: {
-      nuxt: "^3.0.0",
+      nuxt: '^3.0.0',
     },
   },
   defaults: {
     svgo: true,
-    defaultImport: "component",
+    defaultImport: 'component',
   },
-  setup(options) {
-    addVitePlugin(svgLoader(options));
+
+  async setup(options) {
+    addVitePlugin(svgLoader(options))
+
+    // webpack 4/5
+    extendWebpackConfig((config) => {
+      const svgRule = config.module.rules.find((rule) => rule.test.test('.svg'))
+
+      svgRule.test = /\.(png|jpe?g|gif|webp)$/
+
+      config.module.rules.push({
+        test: /\.svg$/,
+        issuer: /\.(vue|js|ts|svg)$/,
+        use: ['vue-loader', 'svg-to-vue-component/loader'],
+        options: {
+          svgoConfig: config.svgo ? config.svgoConfig : config.svgo,
+        },
+      })
+    })
   },
-});
+})
