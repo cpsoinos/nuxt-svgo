1d2361e5068e700676a3bc54567119485bfc0067
Corey Psoinos <coreypsoinos@gmail.com>
2022-11-05 14:03:37 -0400

    test: 💍 correctly test that we can disable svgo

--- test/svgo-disabled.test.ts
+++ test/svgo-disabled.test.ts
@@ -8,7 +8,7 @@ describe('svgo disabled', async () => {
     rootDir: fileURLToPath(new URL('./fixtures', import.meta.url)),
     nuxtConfig: {
       modules: ['nuxt-svgo'],
-      svgoOptions: {
+      svgo: {
         svgo: false
       }
     }
@@ -18,10 +18,8 @@ describe('svgo disabled', async () => {
     // Get response to a server-rendered page with `$fetch`.
     const html = await $fetch('/')
 
-    console.log(html)
-
     expect(html).toContain(
-      '<svg height="24" width="24"><path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732l-7.5-4.33z"></path></svg>'
+      '<svg version="1.1" role="img" height="24" width="24" size="24" viewBox="0 0 24 24"><path d="M13.5 1.515a3 3 0 00-3 0L3 5.845a2 2 0 00-1 1.732V21a1 1 0 001 1h6a1 1 0 001-1v-6h4v6a1 1 0 001 1h6a1 1 0 001-1V7.577a2 2 0 00-1-1.732l-7.5-4.33z"></path></svg>'
     )
   })
 })
