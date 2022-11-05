2db99fe85fe1af3ca2185e4676c9739282bb6583
Corey Psoinos <coreypsoinos@gmail.com>
2022-11-05 14:23:45 -0400

    test: 💍 import module directly instead of using string

--- test/override.test.ts
+++ test/override.test.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath } from 'node:url'
 import { describe, it, expect } from 'vitest'
 import { setup, $fetch } from '@nuxt/test-utils'
+import NuxtSVGO from '../src/module'
 
 describe('default options', async () => {
   await setup({
     rootDir: fileURLToPath(new URL('./fixtures', import.meta.url)),
     nuxtConfig: {
-      modules: ['nuxt-svgo'],
+      modules: [NuxtSVGO],
       svgo: {
         svgoConfig: {
           multipass: true,
--- test/svgo-disabled.test.ts
+++ test/svgo-disabled.test.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath } from 'node:url'
 import { describe, it, expect } from 'vitest'
 import { setup, $fetch } from '@nuxt/test-utils'
+import NuxtSVGO from '../src/module'
 
 describe('svgo disabled', async () => {
   await setup({
     rootDir: fileURLToPath(new URL('./fixtures', import.meta.url)),
     nuxtConfig: {
-      modules: ['nuxt-svgo'],
+      modules: [NuxtSVGO],
       svgo: {
         svgo: false
       }
