7b068fd21dca761c1b7b5b9f118b5e3301d68f39
Javad Mnjd <javad.mnjd@hotmail.com>
2023-04-12 17:25:48 +0330

    fix: update component tests

--- test/component.test.ts
+++ test/component.test.ts
@@ -11,7 +11,7 @@ describe('component', async () => {
     const html = await $fetch('/')
     console.log(html)
     expect(html).toContain(
-      `<span class="nuxt-icon nuxt-icon--fill"><!-- icon with name "nonexisting" not found --></span>`
+      `><path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6"></path></svg>`
     )
   })
 
@@ -20,10 +20,10 @@ describe('component', async () => {
     const html = await $fetch('/')
 
     expect(html).toContain(
-      '<svg width="24" height="24" class="nuxt-icon nuxt-icon--fill"><path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732l-7.5-4.33z"></path></svg>'
+      '><path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732l-7.5-4.33z"></path></svg>'
     )
     expect(html).toContain(
-      '<svg width="24" height="24" class="nuxt-icon nuxt-icon--fill"><path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6"></path></svg>'
+      '><path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6"></path></svg>'
     )
   })
 
@@ -31,7 +31,7 @@ describe('component', async () => {
     const html = await $fetch('/')
     console.log(html)
     expect(html).toContain(
-      `<span class="nuxt-icon nuxt-icon--fill"><!-- icon with name "nonexisting" not found --></span>`
+      `><!-- icon with name "nonexisting" not found --></span>`
     )
   })
 })
