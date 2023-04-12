c1213c4bb9466d4e62c3a496a8bdae40f79529cc
Javad Mnjd <javad.mnjd@hotmail.com>
2023-04-12 17:33:33 +0330

    fix: remove the unnecessary console log

--- test/component.test.ts
+++ test/component.test.ts
@@ -9,7 +9,7 @@ describe('component', async () => {
 
   it('works regardless of `defaultImport` settings', async () => {
     const html = await $fetch('/')
-    console.log(html)
+
     expect(html).toContain(
       `><path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6"></path></svg>`
     )
@@ -29,7 +29,7 @@ describe('component', async () => {
 
   it('it renders an span with a comment saying icon does not exist when icon name is not found', async () => {
     const html = await $fetch('/')
-    console.log(html)
+
     expect(html).toContain(
       `><!-- icon with name "nonexisting" not found --></span>`
     )
