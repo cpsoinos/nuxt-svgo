e9d9c5070ddee4d7b5441ca66615aa331f2ce9d3
javad mnjd <javad.mnjd@hotmail.com>
2023-04-13 20:16:00 +0330

    fix: consolidate script tags to fix ts build error (#94)
    * fix: change style of nuxt-icon component
    
    * fix: lint
    
    * fix: bump version

--- src/runtime/components/nuxt-icon.vue
+++ src/runtime/components/nuxt-icon.vue
@@ -1,5 +1,6 @@
 <template>
-  <IconComponent
+  <component
+    :is="IconComponent"
     :class="{
       'nuxt-icon': fontControlled,
       'nuxt-icon--fill': !filled
@@ -8,7 +9,9 @@
 </template>
 
 <script lang="ts">
-import { h } from '#imports'
+// file imported from https://github.com/gitFoxCode/nuxt-icons/blob/89e53649e5868c31fc97869918ede96504ae1a04/src/runtime/components/nuxt-icon.vue
+// with some modifications
+import { shallowRef, watch, h } from '#imports'
 
 const iconsImport = import.meta.glob('assets/icons/**/**.svg', {
   import: 'default',
@@ -26,40 +29,49 @@ function IconNotFound(name: string) {
     }
   }
 }
-</script>
 
-<script setup lang="ts">
-// file imported from https://github.com/gitFoxCode/nuxt-icons/blob/89e53649e5868c31fc97869918ede96504ae1a04/src/runtime/components/nuxt-icon.vue
-// with some modifications
-import { markRaw, watch } from '#imports'
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    filled: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    fontControlled: {
+      type: Boolean,
+      required: false,
+      default: true
+    }
+  },
+  setup(props) {
+    const IconComponent = shallowRef(
+      iconsImport[`/assets/icons/${props.name}.svg`] || IconNotFound(props.name)
+    )
 
-const props = withDefaults(
-  defineProps<{
-    name: string
-    fontControlled?: boolean
-    filled?: boolean
-  }>(),
-  { filled: false, fontControlled: true }
-)
+    watch(
+      () => props.name,
+      () => {
+        const component = iconsImport[`/assets/icons/${props.name}.svg`]
+        if (!component) {
+          console.error(
+            `[nuxt-svgo] Icon '${props.name}' doesn't exist in 'assets/icons'`
+          )
+          IconComponent.value = IconNotFound(props.name)
+        } else {
+          IconComponent.value = component
+        }
+      }
+    )
 
-const IconComponent = markRaw(
-  iconsImport[`/assets/icons/${props.name}.svg`] || IconNotFound(props.name)
-)
-
-watch(
-  () => props.name,
-  () => {
-    const component = iconsImport[`/assets/icons/${props.name}.svg`]
-    if (!component) {
-      console.error(
-        `[nuxt-svgo] Icon '${props.name}' doesn't exist in 'assets/icons'`
-      )
-      IconComponent.value = IconNotFound(props.name)
-    } else {
-      IconComponent.value = component
+    return {
+      IconComponent
     }
   }
-)
+}
 </script>
 
 <style scoped>
