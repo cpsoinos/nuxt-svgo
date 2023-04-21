300be2062b4d8e8b8a43ffa24211adc98a7e7378
Javad Mnjd <javad.mnjd@hotmail.com>
2023-04-21 05:45:09 +0330

    fix!: add auto import with treeshake
    BREAKING CHANGE: removes the old functionality of nuxt-icon component

--- src/module.ts
+++ src/module.ts
@@ -5,17 +5,15 @@ import {
   addVitePlugin,
   extendWebpackConfig,
   createResolver,
+  addComponentsDir,
   addComponent
 } from '@nuxt/kit'
-import svgLoader from 'vite-svg-loader'
 import type { NuxtModule } from '@nuxt/schema'
+import { SvgLoaderOptions, svgLoader } from './loaders/vite'
 
-type OptimizeOptions = Parameters<typeof svgLoader>[0]['svgoConfig']
-
-export interface ModuleOptions {
-  defaultImport?: 'url' | 'raw' | 'component'
-  svgo?: boolean
-  svgoConfig?: OptimizeOptions
+export type ModuleOptions = SvgLoaderOptions & {
+  autoImportPath?: string
+  simpleAutoImport?: boolean
 }
 
 const nuxtSvgo: NuxtModule<ModuleOptions> = defineNuxtModule({
@@ -30,18 +28,34 @@ const nuxtSvgo: NuxtModule<ModuleOptions> = defineNuxtModule({
   defaults: {
     svgo: true,
     defaultImport: 'component',
-    svgoConfig: {}
+    svgoConfig: {},
+    autoImportPath: './assets/icons/',
+    simpleAutoImport: false
   },
-  setup(options) {
-    const { resolve } = createResolver(import.meta.url)
-
-    addVitePlugin(svgLoader(options))
+  async setup(options) {
+    const { resolvePath, resolve } = createResolver(import.meta.url)
 
     addComponent({
       name: 'nuxt-icon',
-      global: true,
       filePath: resolve('./runtime/components/nuxt-icon.vue')
     })
+
+    addVitePlugin(svgLoader(options))
+
+    if (options.autoImportPath) {
+      addComponentsDir({
+        path: await resolvePath(options.autoImportPath),
+        global: true,
+        extensions: ['svg'],
+        prefix: 'svgo',
+        watch: true,
+        extendComponent(component) {
+          component.filePath =
+            component.filePath +
+            (options.simpleAutoImport ? '?component' : '?componentext')
+        }
+      })
+    }
 
     extendWebpackConfig((config) => {
       // @ts-ignore
--- src/runtime/components/nuxt-icon.vue
+++ src/runtime/components/nuxt-icon.vue
@@ -10,32 +10,10 @@
 
 <script lang="ts">
 // file imported from https://github.com/gitFoxCode/nuxt-icons/blob/89e53649e5868c31fc97869918ede96504ae1a04/src/runtime/components/nuxt-icon.vue
-// with some modifications
-import { shallowRef, watch, h } from '#imports'
-
-const iconsImport = import.meta.glob('assets/icons/**/**.svg', {
-  import: 'default',
-  eager: true,
-  query: {
-    component: ''
-  }
-})
-
-function IconNotFound(name: string) {
-  return {
-    setup() {
-      return () =>
-        h('span', { innerHTML: `<!-- icon with name "${name}" not found -->` })
-    }
-  }
-}
+// with modifications
 
 export default {
   props: {
-    name: {
-      type: String,
-      required: true
-    },
     filled: {
       type: Boolean,
       required: false,
@@ -45,36 +23,16 @@ export default {
       type: Boolean,
       required: false,
       default: true
-    }
-  },
-  setup(props) {
-    const IconComponent = shallowRef(
-      iconsImport[`/assets/icons/${props.name}.svg`] || IconNotFound(props.name)
-    )
-
-    watch(
-      () => props.name,
-      () => {
-        const component = iconsImport[`/assets/icons/${props.name}.svg`]
-        if (!component) {
-          console.error(
-            `[nuxt-svgo] Icon '${props.name}' doesn't exist in 'assets/icons'`
-          )
-          IconComponent.value = IconNotFound(props.name)
-        } else {
-          IconComponent.value = component
-        }
-      }
-    )
-
-    return {
-      IconComponent
+    },
+    IconComponent: {
+      type: Object,
+      required: true
     }
   }
 }
 </script>
 
-<style scoped>
+<style>
 .nuxt-icon {
   width: 1em;
   height: 1em;
