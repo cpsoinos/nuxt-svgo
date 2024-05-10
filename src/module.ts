ca784f23dc5693958d0adde9144edf60a4884da0
Javad Mnjd <javad.mnjd@hotmail.com>
2024-05-10 08:36:07 +0330

    fix: add types to package.json exports and export SvgLoaderOptions

--- src/module.ts
+++ src/module.ts
@@ -11,6 +11,8 @@ import type { NuxtModule } from '@nuxt/schema'
 import type { Config } from 'svgo'
 import { SvgLoaderOptions, svgLoader } from './loaders/vite'
 
+export { SvgLoaderOptions }
+
 /**
  * taken from: https://stackoverflow.com/a/8831937/3542461
  */
