1cf8955df9c6354ea7eb3ae198c5850841ac0915
Corey Psoinos <coreypsoinos@gmail.com>
2026-04-30 17:16:36 -0400

    fix: update svgo to ^3.3.3 to patch CVE GHSA-xpqw-6gx7-v673 (#475)
    fix: update svgo to ^3.3.3 to address CVE GHSA-xpqw-6gx7-v673
    
    Bumps svgo from 3.0.2 to 3.3.3, which fixes a high-severity
    DoS vulnerability via entity expansion in DOCTYPE (Billion Laughs).
    
    Updates test expectations to reflect improved path optimization
    in svgo 3.3.3 (redundant lineto before closepath is now removed).
    
    Closes #473

--- test/customprefix.test.ts
+++ test/customprefix.test.ts
@@ -18,7 +18,7 @@ describe('custom prefix using `componentPrefix`', async () => {
     // Get response to a server-rendered page with `$fetch`.
     const html = await $fetch('/')
     expect(html).toContain(
-      '><path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732l-7.5-4.33z"></path></svg>',
+      '><path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732z"></path></svg>',
     )
     expect(html).toContain(
       '><path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6"></path></svg>',
